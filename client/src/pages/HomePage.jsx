5cd289a14715f3364954b4b31ac9ed9f8c0c0274
akku2005 <asakashsahu20@gmail.com>
2025-03-29 23:32:10 +0530

    changes in HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import Home from "../assets/images/homepage.png";
-import Facility1 from "../assets/images/facility1.png";
+import Facility1 from "../assets/images/facility.png";
 import Facility2 from "../assets/images/facility2.png";
 import Trainer1 from "../assets/images/trainer1.png";
 import Trainer2 from "../assets/images/trainer2.png";
